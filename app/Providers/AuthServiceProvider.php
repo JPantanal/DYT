c8bb6f4cf94000716d22c2e4af758e90c460f597
Jpantanal <johnmorganlewis@gmx.com>
2023-12-08 14:23:52 +0400

    get mail working

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
+use function Laravel\Prompts\outro;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -21,6 +24,19 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        //phpinfo();
+
+        //custom email verification
+        VerifyEmail::toMailusing(function(object $notifiable, string $url){
+            return(new MailMessage)
+                ->greeting('Good Day!')
+            ->subject('Dayton Tutoring Email Verification')
+            ->line('Click the link below to verify your email address.')
+            ->action('Verify Email Address', $url)
+                ->salutation('Regards, \r\n Dayton Tutoring');
+
+        });
     }
+
+
 }
